c2daa92b6ba41f0a3257a81168cf24bb2104f83e
Paul Lotz <paul.lotz@gmx.at>
2025-12-14 12:39:53 +0100

    feat: adapt db schema

--- db/schema.ts
+++ db/schema.ts
@@ -2,7 +2,7 @@ import { date, pgTable, varchar } from "drizzle-orm/pg-core";
 import { resourceId } from "./resource-id";
 
 export const waitlistTable = pgTable("waitlist", {
-  id: resourceId("wait", "id").primaryKey(),
-  email: varchar().notNull().unique(),
-  createdAt: date().notNull().defaultNow(),
+	id: resourceId("wait", "id").primaryKey(),
+	email: varchar().notNull().unique(),
+	createdAt: date().notNull().defaultNow(),
 });
