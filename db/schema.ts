c1976a1c4e834039e4c966089d9e0e8e9845b596
Paul Lotz <paul.lotz@gmx.at>
2025-12-14 12:39:30 +0100

    feat: add proper IDs to schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,8 @@
-import { date, integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { date, pgTable, varchar } from "drizzle-orm/pg-core";
+import { resourceId } from "./resource-id";
 
 export const waitlistTable = pgTable("waitlist", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: resourceId("wait", "id").primaryKey(),
   email: varchar().notNull().unique(),
   createdAt: date().notNull().defaultNow(),
 });
